b68264547c16016f7e00c5934de707a3437567bc
Sicoe Horia-Alexandru <alexisicoe@gmail.com>
2019-02-27 15:28:29 +0200

    implement button

--- my-app/src/Button.tsx
+++ my-app/src/Button.tsx
@@ -1,7 +1,32 @@
 import React, { Component } from 'react'
+import './Button.css'
 
 export default class Button extends Component<any, any> {
   render() {
-    return <div>ceva</div>
+    return (
+      <div>
+        <button className="btn">
+          <ArrowRight />
+          CLICK ME!!
+          <ArrowLeft />
+        </button>
+      </div>
+    )
   }
+}
+
+function ArrowRight() {
+  return (
+    <span className="rightArrow">
+      <div className="fa fa-arrow-right" />
+    </span>
+  )
+}
+
+function ArrowLeft() {
+  return (
+    <span className="leftArrow">
+      <div className="fa fa-arrow-left" />
+    </span>
+  )
 }
